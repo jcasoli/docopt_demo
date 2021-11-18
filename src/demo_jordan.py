d33e6bc1669db97db3c55753d736703c17bafe1b
Jordan Casoli <casolijordan@gmail.com>
2021-11-18 12:34:54 -0800

    Add main function python

--- src/demo_jordan.py
+++ src/demo_jordan.py
@@ -10,9 +10,21 @@ Options:
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 [<arg4>]          Takes any value (this is an optional positional argument)
 """ 
-
 from docopt import docopt
+
 opt = docopt(__doc__)
-print(opt)
-print(opt["<arg4>"])
-print(type(opt))
+# define main function
+def main():
+    # code for "guts" of script goes here
+    print(opt)
+    print(opt["<arg4>"])
+    print(type(opt))
+# code for other functions & tests goes here
+
+# call main function
+if __name__ == "__main__":
+    main()
+
+
+
+
